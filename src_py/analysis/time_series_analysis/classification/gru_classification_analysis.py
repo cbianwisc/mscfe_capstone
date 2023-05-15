7a934df5579e97ec4c4b8195bed70f14085608cb
cbianwisc <cbianwisc@gmail.com>
2023-05-15 15:34:32 -0400

    use class weight to balance output classes

--- src_py/analysis/time_series_analysis/classification/gru_classification_analysis.py
+++ src_py/analysis/time_series_analysis/classification/gru_classification_analysis.py
@@ -7,10 +7,10 @@ from src_py.analysis.time_series_analysis.classification.rnn_classification_anal
 class GruClassificationAnalysis(RnnClassificationAnalysis):
     def model_construction(self, input_shape):
         regressor = Sequential()
-        regressor.add(GRU(units=50, return_sequences=False, input_shape=(input_shape, 1)))
+        regressor.add(GRU(units=16, return_sequences=False, input_shape=(input_shape, 1)))
         regressor.add(Dropout(0.2))
-        regressor.add(Dense(units=3))
-        regressor.add(Activation("softmax"))
+        regressor.add(Dense(units=8, activation="relu"))
+        regressor.add(Dense(units=3, activation="softmax"))
         regressor.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
         return regressor
 
--- src_py/analysis/time_series_analysis/classification/rnn_classification_analysis.py
+++ src_py/analysis/time_series_analysis/classification/rnn_classification_analysis.py
@@ -18,18 +18,26 @@ class RnnClassificationAnalysis(CombinedAnalysis):
         x_train = x_train.fillna(0.0)
         y_train = classify_output_data(self._data_for_train.copy())['overnight_jump']
 
+        class_weight = self.calculate_class_weight(y_train)
         x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
         regressor = self.model_construction(x_train.shape[1])
-        regressor.fit(x_train, y_train, epochs=100, batch_size=100)
         print(regressor.summary())
+        regressor.fit(x_train, y_train, epochs=100, batch_size=300, class_weight=class_weight)
+
         self._model = regressor
+
+    def calculate_class_weight(self, y_train):
+        weight_0 = 1.0 / sum(y_train == 0)
+        weight_1 = 1.0 / sum(y_train == 1)
+        weight_2 = 1.0 / sum(y_train == 2)
+        return {0: weight_0, 1: weight_1, 2: weight_2}
 
     def model_construction(self, input_shape):
         regressor = Sequential()
-        regressor.add(SimpleRNN(units=50, return_sequences=False, input_shape=(input_shape, 1)))
+        regressor.add(SimpleRNN(units=16, return_sequences=False, input_shape=(input_shape, 1)))
         regressor.add(Dropout(0.2))
-        regressor.add(Dense(units=3))
-        regressor.add(Activation("softmax"))
+        regressor.add(Dense(units=8, activation="relu"))
+        regressor.add(Dense(units=3, activation="softmax"))
         regressor.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
         return regressor
 
